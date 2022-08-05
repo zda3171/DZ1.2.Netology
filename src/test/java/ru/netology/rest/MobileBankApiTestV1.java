c0fff6b84971cf6eaf71eb5cc7453485acf2e5db
D.Zubkov <zda3171@gmail.com>
2022-08-05 16:44:00 +0300

    Edit TestV1

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,8 +1,12 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 class MobileBankApiTestV1 {
     @Test
@@ -14,5 +18,65 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 .then()
                 .statusCode(200);
+    }
+    @Test
+    void shouldReturnDemoAccountsContentType() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .contentType(ContentType.JSON);
+    }
+    @Test
+    void shouldReturnDemoAccountsBody() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("", hasSize(3));
+    }
+    @Test
+    void shouldReturnDemoAccountsBodyCurrency() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[0].currency", equalTo("RUB"));
+    }
+    @Test
+    void shouldReturnDemoAccountsBodyBalance() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[1].balance", greaterThanOrEqualTo(0));
+    }
+    @Test
+    void shouldReturnDemoAccountsBodyBalancePositive() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body( "every{ it.balance >= 0 }", is(true));
+    }
+    @Test
+    void shouldReturnDemoAccountsJsonSchema() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
